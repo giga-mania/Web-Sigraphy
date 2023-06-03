05c93b525fbefa0b7aab7f7bf0aed9091e7035ee
giga-mania <hil.giga@gmail.com>
2023-06-03 16:00:40 +0100

    Create helper function getFriendsByUserId

---
+++ src/helpers/getFriendByUserId.ts
@@ -0,0 +1,10 @@
+import {fetchRedis} from "@/helpers/redis";
+
+export const getFriendsByUserId = async (userId: string) => {
+    const friendIds = await fetchRedis('smembers', `user:${userId}:friends`) as string[]
+    return await Promise.all(friendIds.map(async (friendId) => {
+        const friend = await fetchRedis('get', `user:${userId}`) as string
+        return JSON.parse(friend) as User
+    }))
+}
+
